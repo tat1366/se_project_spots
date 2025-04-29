4173c15998143b3e2c227ebfe2cbf3032438dc8e
Tatiana <atilyt2@gmail.com>
2025-04-29 11:12:18 -0400

    editing profilt modal

--- scripts/index.js
+++ scripts/index.js
@@ -26,4 +26,13 @@ const initialCards = [
 ];
 console.log(initialCards);
 const newPostModal = document.querySelector("#new-post-modal");
+
 const editProfileModal = document.querySelector("#edit-profile-modal");
+const editProfileBtn = editProfileModal.querySelector(".profile__edit-btn");
+const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
+const editProfileForm = editProfileModal.querySelector(".modal__form");
+const editProfileNameInput = editProfileModal.querySelector("#name");
+
+editProfileBtn.addEventListener("click", function () {
+  editProfileModal.classList.add("modal_is-opened");
+});
