9de8129f1896e74e4a5b204eb8f19aad20093f37
Tatiana <atilyt2@gmail.com>
2025-05-01 13:05:35 -0400

    making the save buttons functional

--- scripts/index.js
+++ scripts/index.js
@@ -29,21 +29,41 @@ const newPostModal = document.querySelector("#new-post-modal");
 const newPostBtn = document.querySelector(".profile__add-btn");
 const newPostCloseBtn = newPostModal.querySelector(".modal__close-btn");
 const newPostForm = newPostModal.querySelector(".modal__form");
+const newPostImageInput = newPostModal.querySelector("#card-image-input");
+const newPostCaptionInput = newPostModal.querySelector("#post-caption-input");
+
 const editProfileModal = document.querySelector("#edit-profile-modal");
 const editProfileBtn = document.querySelector(".profile__edit-btn");
 const editProfileCloseBtn = editProfileModal.querySelector(".modal__close-btn");
 const editProfileForm = editProfileModal.querySelector(".modal__form");
-
+const editProfileNameInput = editProfileModal.querySelector(
+  "#profile-name-input"
+);
+const editProfileDescriptionInput = editProfileModal.querySelector(
+  "#profile-description-input"
+);
+const editProfileTextEl = editProfileModal.querySelector(".modal__input");
 editProfileBtn.addEventListener("click", function () {
   editProfileModal.classList.add("modal_is-opened");
 });
 editProfileCloseBtn.addEventListener("click", function () {
   editProfileModal.classList.remove("modal_is-opened");
 });
-
+editProfileBtn.addEventListener("click", function () {
+  editProfileModal.classList.remove("modal_is-opened");
+  editProfileNameInput.value = "";
+});
 newPostBtn.addEventListener("click", function () {
   newPostModal.classList.add("modal_is-opened");
 });
 newPostCloseBtn.addEventListener("click", function () {
   newPostModal.classList.remove("modal_is-opened");
+});
+
+const inputValues = {
+  name: editProfileNameInput.value,
+  description: editProfileDescriptionInput.value,
+};
+editProfileForm.addEventListener("submit", function (evt) {
+  evt.preventDefault();
 });
