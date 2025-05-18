3ab7c8787df6de84a12536bd3ec87851a143b961
Tatiana <atilyt2@gmail.com>
2025-05-18 14:04:43 -0400

    sprint 5 stage 1
    iterating through an array objects

--- scripts/index.js
+++ scripts/index.js
@@ -78,3 +78,7 @@ function handleNewPostSubmit(evt) {
 }
 editProfileForm.addEventListener("submit", handleEditProfileSubmit);
 newPostForm.addEventListener("submit", handleNewPostSubmit);
+initialCards.forEach(function (item) {
+  console.log(item.name);
+  console.log(item.link);
+});
